67b3e08b0fd27c147d0d8d3e5d715c0f156ab5bf
dheamonnica <dheamonnica.acc@gmail.com>
2023-02-26 11:55:29 +0700

    login register

--- modules/auth/views/backend/standart/administrator/login.php
+++ modules/auth/views/backend/standart/administrator/login.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?= get_option('site_name') ?> | Log in</title>
+  <title>
+    <?= get_option('site_name') ?> | Log in
+  </title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?= BASE_ASSET ?>admin-lte/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= BASE_ASSET ?>font-awesome-4.5.0/css/font-awesome.min.css">
@@ -22,80 +24,96 @@
 </head>
 
 <body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href=""><b><?= cclang('login'); ?></b> <?= get_option('site_name'); ?></a>
+  <div class="row">
+    <div class="col-lg-6">
+      <img src="https://dummyimage.com/700x700/000/fff" width="100%"/>
     </div>
-    <div class="login-box-body">
-      <p class="login-box-msg"><?= cclang('sign_to_start_your_session'); ?></p>
-      <?php if (isset($error) and !empty($error)) : ?>
-        <div class="callout callout-error">
-          <h4><?= cclang('error'); ?>!</h4>
-          <p><?= $error; ?></p>
+    <div class="col-lg-6" style="background: #a7dda7;">
+      <div class="login-box">
+        <div class="login-logo">
+          <!-- <a href=""><b><?= cclang('login'); ?></b> <?= get_option('site_name'); ?></a> -->
         </div>
-      <?php endif; ?>
-      <?php
-      $message = $this->session->flashdata('f_message');
-      $type = $this->session->flashdata('f_type');
-      if ($message) :
-      ?>
-        <div class="callout callout-<?= $type; ?>">
-          <p><?= $message; ?></p>
-        </div>
-      <?php endif; ?>
-      <?= form_open('', [
-        'name'    => 'form_login',
-        'id'      => 'form_login',
-        'method'  => 'POST'
-      ]); ?>
-      <div class="form-group has-feedback <?= form_error('username') ? 'has-error' : ''; ?>">
-        <input type="email" class="form-control" placeholder="Email" name="username" value="<?= set_value('username', 'admin@admin.com'); ?>">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback <?= form_error('password') ? 'has-error' : ''; ?>">
-        <input type="password" class="form-control" placeholder="Password" name="password" value="admin123">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
+        <div class="login-box-body">
+          <h1>Login</h1>
+          <!-- <p class="login-box-msg"><?= cclang('sign_to_start_your_session'); ?></p> -->
+          <?php if (isset($error) and !empty($error)): ?>
+            <div class="callout callout-error">
+              <h4>
+                <?= cclang('error'); ?>!
+              </h4>
+              <p>
+                <?= $error; ?>
+              </p>
+            </div>
+          <?php endif; ?>
+          <?php
+          $message = $this->session->flashdata('f_message');
+          $type = $this->session->flashdata('f_type');
+          if ($message):
+            ?>
+            <div class="callout callout-<?= $type; ?>">
+              <p>
+                <?= $message; ?>
+              </p>
+            </div>
+          <?php endif; ?>
+          <?= form_open('', [
+            'name' => 'form_login',
+            'id' => 'form_login',
+            'method' => 'POST'
+          ]); ?>
+          <div class="form-group has-feedback <?= form_error('username') ? 'has-error' : ''; ?>">
+            <input type="email" class="form-control" placeholder="Email" name="username"
+              value="<?= set_value('username', 'admin@admin.com'); ?>">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback <?= form_error('password') ? 'has-error' : ''; ?>">
+            <input type="password" class="form-control" placeholder="Password" name="password" value="admin123">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <!-- <div class="col-xs-8">
           <div class="checkbox icheck">
             <label>
               <input type="checkbox" name="remember" value="1"> <?= cclang('remember_me'); ?>
             </label>
           </div>
-        </div>
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat"><?= cclang('sign_in'); ?></button>
-        </div>
-      </div>
-      <?= form_close(); ?>
+        </div> -->
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-success">Masuk</button>
+            </div>
+          </div>
+          <?= form_close(); ?>
+          <br>
 
-      <a href="<?= site_url('administrator/forgot-password'); ?>"><?= cclang('i_forgot_my_password'); ?></a><br>
-      <a href="<?= site_url('administrator/register'); ?>" class="text-center"><?= cclang('register_a_new_membership'); ?></a>
+          <!-- <a href="<?= site_url('administrator/forgot-password'); ?>"><?= cclang('i_forgot_my_password'); ?></a><br> -->
+          Belum Punya Akun?
+          <a href="<?= site_url('administrator/register'); ?>" class="text-center">Registrasi</a>
 
-      <br>
+          <!-- <br>
       <br>
       <p align="center"><b>-<?= cclang('or') ?>-</b></p>
-      <a href="<?= site_url('oauth/v/google'); ?>" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google"></i> <?= cclang('sign_in_using') ?> Google+</a>
+      <a href="<?= site_url('oauth/v/google'); ?>" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google"></i> <?= cclang('sign_in_using') ?> Google+</a> -->
 
+        </div>
+      </div>
     </div>
-  </div>
 
-  <script src="<?= BASE_ASSET ?>admin-lte/plugins/jQuery/jquery-3.6.0.min.js"></script>
-  <script src="<?= BASE_ASSET ?>admin-lte/bootstrap/js/bootstrap.min.js"></script>
-  <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
-  <script>
-    $(function() {
+    <script src="<?= BASE_ASSET ?>admin-lte/plugins/jQuery/jquery-3.6.0.min.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
+    <script>
+      $(function () {
 
-      "use strict";
+        "use strict";
 
-      $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%'
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-blue',
+          radioClass: 'iradio_square-blue',
+          increaseArea: '20%'
+        });
       });
-    });
-  </script>
+    </script>
 
 </body>
 
--- modules/auth/views/backend/standart/administrator/register_member.php
+++ modules/auth/views/backend/standart/administrator/register_member.php
@@ -4,7 +4,9 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title><?= get_option('site_name') ?> | Log in</title>
+  <title>
+    <?= get_option('site_name') ?> | Log in
+  </title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?= BASE_ASSET ?>admin-lte/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?= BASE_ASSET ?>font-awesome-4.5.0/css/font-awesome.min.css">
@@ -22,82 +24,105 @@
 </head>
 
 <body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href="#"><b><?= cclang('register'); ?></b> </a>
+  <div class="row">
+    <div class="col-lg-6">
+      <img src="https://dummyimage.com/700x700/000/fff" width="100%"/>
     </div>
-    <div class="login-box-body">
-      <p class="login-box-msg"><?= cclang('register_a_new_membership'); ?></p>
-      <?php if (isset($error) and !empty($error)) : ?>
-        <div class="callout callout-error">
-          <h4><?= cclang('error'); ?>!</h4>
-          <p><?= $error; ?></p>
+    <div class="col-lg-6" style="background: #a7dda7;">
+      <div class="register-box">
+        <div class="login-logo">
+          <a href="#"><b>
+              <!-- <?= cclang('register'); ?> -->
+            </b> </a>
         </div>
-      <?php endif; ?>
-      <?= form_open('', [
-        'name'    => 'form_login',
-        'id'      => 'form_login',
-        'method'  => 'POST'
-      ]); ?>
-      <div class="form-group has-feedback <?= form_error('full_name') ? 'has-error' : ''; ?>">
-        <label>Full Name </label>
-        <input class="form-control" placeholder="Full Name" name="full_name" value="<?= set_value('full_name'); ?>">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback <?= form_error('username') ? 'has-error' : ''; ?>">
-        <label>Username <span class="required">*</span> </label>
-        <input class="form-control" placeholder="Username" name="username" value="<?= set_value('username'); ?>">
-        <span class="glyphicon glyphicon-user form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback <?= form_error('email') ? 'has-error' : ''; ?>">
-        <label>Email <span class="required">*</span> </label>
-        <input type="email" class="form-control" placeholder="Email" name="email" value="<?= set_value('email'); ?>">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-      </div>
-      <div class="form-group has-feedback <?= form_error('password') ? 'has-error' : ''; ?>">
-        <label>Password <span class="required">*</span> </label>
-        <input type="password" class="form-control" placeholder="Password" name="password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-      </div>
-      <?php $cap = get_captcha(); ?>
-      <div class="form-group <?= form_error('email') ? 'has-error' : ''; ?>">
-        <label><?= cclang('human_challenge'); ?> <span class="required">*</span> </label>
-        <div class="captcha-box" data-captcha-time="<?= $cap['time']; ?>">
-          <input type="text" name="captcha" placeholder="">
-          <a class="btn btn-flat  refresh-captcha  "><i class="fa fa-refresh text-danger"></i></a>
-          <span class="box-image"><?= $cap['image']; ?></span>
-        </div>
-      </div>
-      <small class="info help-block">
-      </small>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox" name="agree" value="1"> <?= cclang('i_agree_to_the_terms'); ?>
-            </label>
+        <div class="login-box-body">
+          <!-- <p class="login-box-msg">
+            <?= cclang('register_a_new_membership'); ?>
+          </p> -->
+          <h1>Registrasi Akun</h1><br>
+          <?php if (isset($error) and !empty($error)): ?>
+            <div class="callout callout-error">
+              <h4>
+                <?= cclang('error'); ?>!
+              </h4>
+              <p>
+                <?= $error; ?>
+              </p>
+            </div>
+          <?php endif; ?>
+          <?= form_open('', [
+            'name' => 'form_login',
+            'id' => 'form_login',
+            'method' => 'POST'
+          ]); ?>
+          <div class="form-group has-feedback <?= form_error('full_name') ? 'has-error' : ''; ?>">
+            <label>Full Name </label>
+            <input class="form-control" placeholder="Full Name" name="full_name" value="<?= set_value('full_name'); ?>">
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
           </div>
-        </div>
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat"><?= cclang('register'); ?></button>
+          <!-- <div class="form-group has-feedback <?= form_error('username') ? 'has-error' : ''; ?>">
+            <label>Username <span class="required">*</span> </label>
+            <input class="form-control" placeholder="Username" name="username" value="<?= set_value('username'); ?>">
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+          </div> -->
+          <div class="form-group has-feedback <?= form_error('email') ? 'has-error' : ''; ?>">
+            <label>Email <span class="required">*</span> </label>
+            <input type="email" class="form-control" placeholder="Email" name="email"
+              value="<?= set_value('email'); ?>">
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback <?= form_error('password') ? 'has-error' : ''; ?>">
+            <label>Password <span class="required">*</span> </label>
+            <input type="password" class="form-control" placeholder="Password" name="password">
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <?php $cap = get_captcha(); ?>
+          <div class="form-group <?= form_error('email') ? 'has-error' : ''; ?>">
+            <!-- <label>
+              <?= cclang('human_challenge'); ?> <span class="required">*</span>
+            </label>
+            <div class="captcha-box" data-captcha-time="<?= $cap['time']; ?>">
+              <input type="text" name="captcha" placeholder="">
+              <a class="btn btn-flat  refresh-captcha  "><i class="fa fa-refresh text-danger"></i></a>
+              <span class="box-image">
+                <?= $cap['image']; ?>
+              </span>
+            </div> -->
+          </div>
+          <small class="info help-block">
+          </small>
+          <div class="row">
+            <!-- <div class="col-xs-8">
+              <div class="checkbox icheck">
+                <label>
+                  <input type="checkbox" name="agree" value="1">
+                  <?= cclang('i_agree_to_the_terms'); ?>
+                </label>
+              </div>
+            </div> -->
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-success">
+                Daftar
+              </button>
+            </div>
+          </div>
+          <?= form_close(); ?>
+          <br>
+          Sudah Punya Akun?<a href="<?= site_url('administrator/login'); ?>" class="text-center"> Masuk</a>
+
         </div>
       </div>
-      <?= form_close(); ?>
-
-      <a href="<?= site_url('administrator/login'); ?>" class="text-center"><?= cclang('i_already_a_new_membership'); ?></a>
-
     </div>
-  </div>
 
-  <script>
-    var BASE_URL = "<?= base_url(); ?>";
-  </script>
+    <script>
+      var BASE_URL = "<?= base_url(); ?>";
+    </script>
 
-  <script src="<?= BASE_ASSET ?>admin-lte/plugins/jQuery/jquery-3.6.0.min.js"></script>
-  <script src="<?= BASE_ASSET ?>admin-lte/bootstrap/js/bootstrap.min.js"></script>
-  <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
-  <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
-  <script src="<?= BASE_ASSET ?>js/page/auth/register-member.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/plugins/jQuery/jquery-3.6.0.min.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
+    <script src="<?= BASE_ASSET ?>admin-lte/plugins/iCheck/icheck.min.js"></script>
+    <script src="<?= BASE_ASSET ?>js/page/auth/register-member.js"></script>
 
 </body>
 
